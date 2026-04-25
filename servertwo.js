2efd06ce89f8af7f2f72fcbdebef1cfcc0265ec7
Emperror <danieldalpiva445@gmail.com>
2026-04-25 20:59:36 -0300

    feat: Primeiro Servidor

---
+++ servertwo.js
@@ -0,0 +1,26 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ mensagem: 'Página inicial' }))
+  } else if (req.url === '/sobre') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ nome: 'Daniel', empresa: 'Questor' }))
+  } else if (req.url === '/tecnologias') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify([
+      { id: 1, nome: 'React' },
+      { id: 2, nome: 'Node.js' },
+      { id: 3, nome: 'PHP' }
+    ]))
+  } else {
+    res.writeHead(404)
+    res.end(JSON.stringify({ erro: 'Rota não encontrada' }))
+  }
+})
+
+server.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000')
+})
